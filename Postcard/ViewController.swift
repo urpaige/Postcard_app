26bdfd41475857893531764b34d15106d23a6abb
HAROLD PAIGE <haroldpaige@haroldpaigesmbp.attlocal.net>
2014-12-16 11:51:23 -0600

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by HAROLD PAIGE on 12/16/14.
+//  Copyright (c) 2014 HAROLD PAIGE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
